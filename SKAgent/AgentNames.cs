5b7cd6e076131d335aca31d0ed86355c706212dc
Kazuki Ota <kaota@microsoft.com>
2025-06-28 16:37:14 +0900

    不要なusingを削除

--- SKAgent/AgentNames.cs
+++ SKAgent/AgentNames.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SKAgent;
+﻿namespace SKAgent;
 public class AgentNames
 {
     public const string Researcher = nameof(Researcher);
